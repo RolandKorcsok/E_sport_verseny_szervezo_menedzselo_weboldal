5364a6e247852c68dcc6ff1b9a3e534315a826e1
Korcsok Roland József <korcsokrolandjozsef-30143@taszi.hu>
2025-05-29 09:19:00 +0200

    Leave Team bug fixes

--- Project/e_Sport_Projekt/backend/controllers/organizerController.js
+++ Project/e_Sport_Projekt/backend/controllers/organizerController.js
@@ -420,7 +420,7 @@ const organizerUpdate = async (req, res) => {
 
         let date = new Date();
 
-        if(!validator.isEmail(new_email_address)){
+        if(new_email_address && !validator.isEmail(new_email_address)){
             return res.status(400).json({message: "A megadott e-mail cím nem e-mail cím!"});
         }
 
